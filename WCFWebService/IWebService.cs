6b96db402dac0b3fb8c6a3e25801e4abdf308b5f
dpoarch <davidpoarch@outlook.com>
2017-12-14 21:12:05 +0800

    Initial Commit

---
+++ WCFWebService/IWebService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFWebService
+{
+    [ServiceContract]
+    public interface IWebService
+    {
+        [OperationContract]
+        List<Person> GetAllPersons();
+        Person GetPersonById(int id);
+        string Create(string firstname, string lastname, int age, string gender);
+        string Update(int id, string firstname, string lastname, int age, string gender);
+        string Delete(int id);
+    }
+}
---
+++ WCFWebService/WebService.svc.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFWebService
+{
+    public class WebService : IWebService
+    {
+
+        public dbEntities db = new dbEntities();
+
+        public string Create(string firstname, string lastname, int age, string gender)
+        {
+            try
+            {
+                Person person = new Person();
+                person.Firstname = firstname;
+                person.Lastname = lastname;
+                person.Age = age;
+                person.Gender = gender;
+                db.People.Add(person);
+                db.SaveChanges();
+
+                return "Success";
+
+            }
+            catch (Exception e) {
+                return "Failed";
+            }
+
+        }
+
+        public string Delete(int id)
+        {
+            try
+            {
+                Person person = db.People.FirstOrDefault(i => i.Id == id);
+                db.People.Attach(person);
+                db.People.Remove(person);
+                db.SaveChanges();
+
+                return "Success";
+            }
+            catch (Exception e) {
+                return "Failed";
+            }
+
+        }
+
+        public List<Person> GetAllPersons()
+        {
+            List<Person> personList = db.People.ToList();
+            return personList;
+        }
+
+        public Person GetPersonById(int id)
+        {
+            Person person = db.People.FirstOrDefault(i => i.Id == id);
+            return person;
+        }
+
+        public string Update(int id, string firstname, string lastname, int age, string gender)
+        {
+            try
+            {
+                Person person = db.People.FirstOrDefault(i => i.Id == id);
+                person.Firstname = firstname;
+                person.Lastname = lastname;
+                person.Age = age;
+                person.Gender = gender;
+                db.SaveChanges();
+
+                return "Success";
+            }
+            catch (Exception e)
+            {
+                return "Failed";
+            }
+        }
+    }
+}
